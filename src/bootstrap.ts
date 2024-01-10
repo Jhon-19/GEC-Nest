854b65dfa4eecda188fe656bd24956174ca482e0
Jhony <1723368058@qq.com>
2024-01-10 09:45:20 +0800

    add global validation pipe

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,11 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './shared/services/app-config.service';
+import {
+  HttpStatus,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(AppConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      exceptionFactory: (errors) =>
+        new UnprocessableEntityException(
+          errors.map((e) => {
+            const rule = Object.keys(e.constraints)[0];
+            const msg = e.constraints[rule];
+            return `property ${e.property} validation failed: ${msg}, following constraints: ${rule}`;
+          })[0],
+        ),
+    }),
+  );
 
   const { port } = configService.appConfig;
 
